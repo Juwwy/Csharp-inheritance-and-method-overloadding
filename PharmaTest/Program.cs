b20c5ea86fc682e2790b72091ce723141310c116
Olowu Juwon <juwonolowu52@gmail.com>
2021-04-08 07:47:50 +0100

    Csharp inheritance with method overloading

--- PharmaTest/Program.cs
+++ PharmaTest/Program.cs
@@ -11,6 +11,7 @@ namespace PharmaTest
             Medicine medStore = new Medicine(126464, "Vitamin-C", "Tuyil", 2000M, 40, manufDate, expDate, 0076);
             Sales saleDetails = new Sales(1223, 40, 340, 105, "Lagos");
 
+
             Console.WriteLine($"{medStore.Print(1277)}");
             Console.WriteLine($"{medStore.Print(2334, "Bunto")}");
             Console.WriteLine($"{saleDetails.Display()}");
